da41c1819503cf3ae7e7888657b708455bcd72c8
Neeraj Patil <patilneeraj2212@gmail.com>
2023-08-27 23:24:05 +0530

    Setter and field injection

--- spring-demo-annotations/src/main/java/com/learnspring/annotations/beans/TennisCoach.java
+++ spring-demo-annotations/src/main/java/com/learnspring/annotations/beans/TennisCoach.java
@@ -6,12 +6,33 @@ import org.springframework.stereotype.Component;
 @Component
 public class TennisCoach implements Coach {
 
+    // Field Injection
+    @Autowired
     private FortuneService fortuneService;
 
-    @Autowired
-    public TennisCoach(FortuneService fortuneService) {
-        this.fortuneService = fortuneService;
+    // Constructor injection
+    // @Autowired
+    // public TennisCoach(FortuneService fortuneService) {
+    // this.fortuneService = fortuneService;
+    // }
+
+    public TennisCoach() {
+        System.out.println(">> TennisCoach: inside default constructor");
     }
+
+    // Setter injection
+    // @Autowired
+    // public void setFortuneService(FortuneService fortuneService) {
+    // System.out.println(">> TennisCoach: inside setter fortuneservice");
+    // this.fortuneService = fortuneService;
+    // }
+
+    // Method Inejection
+    // @Autowired
+    // public void doSomeCrazyStuff(FortuneService fortuneService) {
+    // System.out.println(">> TennisCoach: inside doSomeCrazyStuff");
+    // this.fortuneService = fortuneService;
+    // }
 
     @Override
     public String getDailyWorkout() {
